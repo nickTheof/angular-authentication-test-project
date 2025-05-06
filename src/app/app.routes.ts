ee536beb6f8c8cb98edfdafe7693949f2896e340
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-06 13:36:11 +0300

    add initial mockup and Landing page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { DashboardComponent } from './components/protected/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: WelcomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: RegisterComponent,
+  },
+  {
+    path: 'page-not-found',
+    component: NotFoundComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'page-not-found',
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+];
---
+++ src/app/components/welcome/welcome.component.ts
@@ -0,0 +1,10 @@
+import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-welcome',
+  imports: [RouterLink],
+  templateUrl: './welcome.component.html',
+  styleUrl: './welcome.component.css',
+})
+export class WelcomeComponent {}
