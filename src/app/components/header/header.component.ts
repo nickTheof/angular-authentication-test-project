b1ece1352c45458f8ae3adf4bed63eb81ea88def
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-07 13:11:44 +0300

    add current user email and logout functionality in header for authenticated users

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
+  private authService = inject(AuthService);
+  currentUser = this.authService.user;
 
+  logoutUser() {
+    this.authService.logout();
+  }
 }
